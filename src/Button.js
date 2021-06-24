7373cb5495a607319383474bd80e2526073adb42
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-24 03:22:31 -0700

    styled the buttons

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,8 @@
+import "./button.css";
+
 const Button = ({ colorType, children, id }) => {
   return (
-    <button id={id} className={"button" + colorType}>
+    <button id={id} className={`button button--${colorType}`}>
       {children}
     </button>
   );
