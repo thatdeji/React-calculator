1af59f4775ef128b1361467dacf41ffbdc3b9791
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-24 03:10:00 -0700

    general styling

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ function App() {
   return (
     <div className="App">
       <div className="calculator">
-        <div className="display" />
-        {buttons.map(({ id, colorType, value }) => (
-          <Button colorType={colorType} id={id} key={id}>
-            {value}
-          </Button>
-        ))}
+        <div className="display">ss</div>
+        <div className="operators">
+          {buttons.map(({ id, colorType, value }) => (
+            <Button colorType={colorType} id={id} key={id}>
+              {value}
+            </Button>
+          ))}
+        </div>
       </div>
     </div>
   );
