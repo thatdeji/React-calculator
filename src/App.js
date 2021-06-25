2a63e744c83083a14919210f5a43dbc71af8f5a2
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-25 04:38:09 -0700

    clean code

--- src/App.js
+++ src/App.js
@@ -1,71 +1,11 @@
 import React from "react";
 import Calculator from "./features/calculator/Calculator";
-// import Button from "./Button";
-// import { buttons } from "./data/buttons";
-// import {
-//   selectOutput,
-//   selectHistory,
-//   numberClick,
-//   outputNegate,
-//   calculatorReset,
-//   outputDecimal,
-//   computeOperatorThunk,
-//   computeEqualThunk
-// } from "./features/calculator/calculatorSlice";
-// import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  //   const dispatch = useDispatch();
-  //   const output = useSelector(selectOutput);
-  //   const history = useSelector(selectHistory);
-
-  //   const handleClick = (value, type) => {
-  //     switch (type) {
-  //       case "number":
-  //         dispatch(numberClick(value));
-  //         break;
-  //       case "operator":
-  //         dispatch(computeOperatorThunk(value));
-  //         break;
-  //       case "equals":
-  //         dispatch(computeEqualThunk());
-  //         break;
-  //       case "negate":
-  //         dispatch(outputNegate());
-  //         break;
-  //       case "clear":
-  //         dispatch(calculatorReset());
-  //         break;
-  //       case "decimal":
-  //         dispatch(outputDecimal());
-  //         break;
-  //       default:
-  //         return;
-  //     }
-  //   };
   return (
     <div className="App">
       <div className="calculator-container">
         <Calculator />
-        {/*<div id="display" className="display">
-          <div className="display__container">
-            <div className="history">{history}</div>
-            <div className="output">{output}</div>
-          </div>
-        </div>
-        <div className="operators">
-          {buttons.map(({ id, colorType, value, type }) => (
-            <Button
-              onClick={handleClick}
-              colorType={colorType}
-              id={id}
-              key={id}
-              type={type}
-            >
-              {value}
-            </Button>
-          ))}
-        </div>*/}
       </div>
     </div>
   );
