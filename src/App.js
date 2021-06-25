07f2801dea6e011028a10009909ee698b8791204
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-25 10:59:47 -0700

    refactored code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Calculator from "./features/calculator/Calculator";
 import Toggle from "./features/toggle/Toggle";
 import { selectToggle } from "./features/toggle/toggleSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { variablesLightMode, variablesDarkMode } from "./data/variables";
+import { setRootVariables } from "./common/utils";
 
 function App() {
   const toggleState = useSelector(selectToggle);
@@ -10,37 +12,9 @@ function App() {
   useEffect(
     () => {
       if (!toggleState) {
-        document
-          .querySelector(":root")
-          .style.setProperty("--calculator-bg", "#22252d");
-        document
-          .querySelector(":root")
-          .style.setProperty("--operator-bg", "#2a2d37");
-        document
-          .querySelector(":root")
-          .style.setProperty("--button-bg", "#282b33");
-        document
-          .querySelector(":root")
-          .style.setProperty("--button-color-primary", "#fafafd");
-        document
-          .querySelector(":root")
-          .style.setProperty("--app-bg", "#f5f3f3");
+        setRootVariables(variablesDarkMode);
       } else {
-        document
-          .querySelector(":root")
-          .style.setProperty("--calculator-bg", "#ffffff");
-        document
-          .querySelector(":root")
-          .style.setProperty("--operator-bg", "#f9f9f9");
-        document
-          .querySelector(":root")
-          .style.setProperty("--button-bg", "#f6f7f7");
-        document
-          .querySelector(":root")
-          .style.setProperty("--button-color-primary", "#3a3b3f");
-        document
-          .querySelector(":root")
-          .style.setProperty("--app-bg", "#414349");
+        setRootVariables(variablesLightMode);
       }
     },
     [toggleState]
---
+++ src/data/variables.js
@@ -0,0 +1,45 @@
+export const variablesDarkMode = [
+  {
+    name: "--calculator-bg",
+    value: "#22252d"
+  },
+  {
+    name: "--operator-bg",
+    value: "#2a2d37"
+  },
+  {
+    name: "--button-bg",
+    value: "#282b33"
+  },
+  {
+    name: "--button-color-primary",
+    value: "#fafafd"
+  },
+  {
+    name: "--app-bg",
+    value: "#f5f3f3"
+  }
+];
+
+export const variablesLightMode = [
+  {
+    name: "--calculator-bg",
+    value: "#ffffff"
+  },
+  {
+    name: "--operator-bg",
+    value: "#f9f9f9"
+  },
+  {
+    name: "--button-bg",
+    value: "#f6f7f7"
+  },
+  {
+    name: "--button-color-primary",
+    value: "#3a3b3f"
+  },
+  {
+    name: "--app-bg",
+    value: "#414349"
+  }
+];
