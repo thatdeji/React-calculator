6031351c41da53f6d84a44b247405e57aedea923
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-25 09:32:39 -0700

    update styles

--- src/features/Toggle/Toggle.js
+++ src/features/Toggle/Toggle.js
@@ -2,17 +2,19 @@ import "./toggle.css";
 
 const Toggle = () => {
   return (
-    <div className="toggle">
-      <div className="toggle__track">
-        <div className="toggle__mode toggle__mode--dark">🌜</div>
-        <div className="toggle__mode toggle__mode--light">🌞</div>
+    <div className="toggle-container">
+      <div className="toggle">
+        <div className="toggle__track">
+          <div className="toggle__mode toggle__mode--dark">🌜</div>
+          <div className="toggle__mode toggle__mode--light">🌞</div>
+        </div>
+        <div className="toggle__thumb" />
+        <input
+          type="checkbox"
+          className="toggle__screen-readers"
+          aria-label="Toggle between light and dark mode"
+        />
       </div>
-      <div className="toggle__thumb" />
-      <input
-        type="checkbox"
-        className="toggle__screen-readers"
-        aria-label="Toggle between light and dark mode"
-      />
     </div>
   );
 };
