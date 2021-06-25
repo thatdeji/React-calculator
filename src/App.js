8000fb8e6945839b399d8c0068149d9eef39ce2a
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-25 09:26:22 -0700

    updated toggle styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Calculator from "./features/calculator/Calculator";
+import Toggle from "./features/toggle/Toggle";
 
 function App() {
   return (
     <div className="App">
       <div className="calculator-container">
+        <Toggle />
         <Calculator />
       </div>
     </div>
