0f6a65ee0a1ed611830ef6c23b60dd5c32d2538a
Akindeji <akindejioluwaseun015@gmail.com>
2021-06-25 09:39:29 -0700

    added toggle to global store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import calculatorReducer from "../features/calculator/calculatorSlice";
+import toggleReducer from "../features/toggle/toggleSlice";
 
 export const store = configureStore({
   reducer: {
-    calculator: calculatorReducer
+    calculator: calculatorReducer,
+    toggle: toggleReducer
   }
 });
